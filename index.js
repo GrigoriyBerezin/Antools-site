2c674b5a64edc01129f7aefa522db44035f3a2ef
Grigory <grishka5556@gmail.com>
2022-12-28 19:32:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const spans = document.querySelectorAll(".popular__icon")
+// spans.forEach((span) => {
+//     span.addEventListener("click", () => {
+//         spans.forEach((sp) => {
+//             if (sp !== span) {
+//                 sp.classList.remove("popular__icon_red")
+//                 sp.previousElementSibling.setAttribute("checked", "false")
+//             }
+//         })
+//         span.classList.toggle("popular__icon_red")
+//         span.previousElementSibling.setAttribute("checked", "true")
+//     })
+// })
